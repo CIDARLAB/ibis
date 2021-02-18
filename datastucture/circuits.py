f52c06264065b152c205ff3b7bfc2938276cef9c
W.R. Jackson <jackson@justjackson.dev>
2021-02-18 10:14:44 -0500

    Showing how we are doing things, addition of tasks in main.py

--- datastucture/circuits.py
+++ datastucture/circuits.py
@@ -6,3 +6,7 @@ Datastructure describing the various attributes of a Genetic Circuit
 Written by W.R. Jackson, Ben Bremer, Eric South
 --------------------------------------------------------------------------------
 '''
+
+
+class GeneticCircuit:
+    pass
