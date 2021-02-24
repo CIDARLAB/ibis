d07ad9695b356201e21fb227ff179c3fe374ac47
W.R. Jackson <jackson@justjackson.dev>
2021-02-24 14:30:26 -0500

    Trivial example of parsing working for initial basecase.

---
+++ tests/test_ingress.py
@@ -0,0 +1,15 @@
+"""
+--------------------------------------------------------------------------------
+Description:
+Tests for the Ingress Module
+
+Written by W.R. Jackson, Ben Bremer, Eric South
+--------------------------------------------------------------------------------
+"""
+from ingress import parse_sbol_xml_tree
+
+
+def test_ingress_module():
+    input_file = "tests/test_circuits/example_and_gate.xml"
+    gc = parse_sbol_xml_tree(input_file)
+    print(gc)
