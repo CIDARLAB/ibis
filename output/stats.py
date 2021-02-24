fdb2f23f127b53f403b5a0b8c392c7ab56b33c36
W.R. Jackson <jackson@justjackson.dev>
2021-02-24 14:54:55 -0500

    Any color you like as long as it's black

--- output/stats.py
+++ output/stats.py
@@ -1,8 +1,8 @@
-'''
+"""
 --------------------------------------------------------------------------------
 Description:
 Generates a statistical report for genetic circuit scoring
 
 Written by W.R. Jackson, Ben Bremer, Eric South
 --------------------------------------------------------------------------------
-'''
+"""
